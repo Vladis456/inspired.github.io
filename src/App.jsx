54f7d17a32902d84edd68cbe734b262b43f1a3ac
Vladis456 <m.obey@bk.ru>
2023-07-03 16:40:21 +0300

    top and footer components done

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Header } from "./Components/Header/Header.jsx";
+import { Footer } from "./Components/Footer/Footer.jsx";
+export const App = () => (
+  <>
+    <Header/>
+    <Footer/>
+  </>
+)
+
+
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import { Top } from "./Top/Top.jsx";
+import { Navigation } from "./Navigation/Navigation.jsx";
+
+
+export const Header = () => (
+	<header>
+		<Top />
+		<Navigation />
+	</header>
+)
